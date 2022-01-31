2b0d341b2daa8783bfb9cd127bce903b51999281
Lagan Gupta <53617100+Lagan28@users.noreply.github.com>
2022-02-01 01:12:56 +0530

    dishes component added

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/menuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div>
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Hello</NavbarBrand>
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            dishes: DISHES
+        };
+    }
+
+    render(){
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Hello</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes} />
             </div>
-        </Navbar>
-        <Menu />
-    </div>
-  );
+        );
+    }
+
 }
 
 export default App;
