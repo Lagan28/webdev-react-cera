95c315a8c805a8274907c0a091fd11e0def42e26
Lagan Gupta <53617100+Lagan28@users.noreply.github.com>
2022-02-04 01:12:11 +0530

    header and footer added

--- confusion/src/components/dishDetailComponent.js
+++ confusion/src/components/dishDetailComponent.js
@@ -38,37 +38,35 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         }
     }
 
-    function RenderComm({comments}) {
-
-        if (comments == null) {
-            return (
-                <div></div>
-            );
-        }
-        const comms = comments.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>--{comment.author},
-                        &nbsp;
-                        {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
-                    </p>
-                </li>
-            )
-        })
-        return (
-            <div className='col-12 col-md-5 m-1'>
-                <h4> Comments </h4>
-                <ul className='list-unstyled'>
-                    {comms}
-                </ul>
-            </div>
-        )
+function RenderComments({comments}){
+    if (comments == null) {
+        return (<div></div>)
     }
+    const cmnts = comments.map(comment => {
+        return (
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author},
+                    &nbsp;
+                    {new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit'
+                    }).format(new Date(comment.date))}
+                </p>
+            </li>
+        )
+    })
+    return (
+        <div className='col-12 col-md-5 m-1'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {cmnts}
+            </ul>
+
+        </div>
+    )
+}
 
     const DishDetail = (props) => {
 
@@ -88,8 +86,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         return (
             <div className="container">
                 <div className="row">
-                    <RenderDish dish={props.dish}/>
-                    <RenderComm comments={props.comments}/>
+                    <RenderDish dish={ props.dish }/>
+                    <RenderComments comments={ props.comments }/>
                 </div>
             </div>
         );
