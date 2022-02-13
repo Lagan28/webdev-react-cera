64f2d5297140b0cea3a6faaa1305617899f7ec65
Lagan Gupta <53617100+Lagan28@users.noreply.github.com>
2022-02-13 17:26:41 +0530

    created action creators for dishes

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +13,10 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+
+        //"thunks" are a pattern of writing functions with logic inside that can interact with a Redux store's dispatch and getState methods
+        applyMiddleware(thunk, logger)
     );
 
     return store;
